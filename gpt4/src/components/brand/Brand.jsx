d7afe9692d40fb3c0dd8e7f23c5bc7ec0ef4d336
afshinarshad <afshinarshad@gmail.com>
2023-05-02 19:53:37 +0330

    brand

--- gpt4/src/components/brand/Brand.jsx
+++ gpt4/src/components/brand/Brand.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import './brand.css'
+import React from "react";
+import "./brand.css";
+import { google, slack, dropbox, shopify, atlassian } from "./imports";
 
 const Brand = () => {
   return (
-    <div>Brand</div>
-  )
-}
+    <div className="gpt4__brand section__padding">
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian" />
+      </div>
+    </div>
+  );
+};
 
-export default Brand
+export default Brand;
